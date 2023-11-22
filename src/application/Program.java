35522155322b23a44cd8cfe6cb5231c070a80e14
Thiago Scavello <scavellothiago@gmail.com>
2023-11-21 21:31:31 -0300

    Placing pieces on the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,14 @@
 package application;
 
 import boardgame.Board;
+import chess.ChessMath;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Board board = new Board(0, 0);
+		ChessMath chessMath = new ChessMath();
+		UI.printBoard(chessMath.getPieces());
 
 	}
 
